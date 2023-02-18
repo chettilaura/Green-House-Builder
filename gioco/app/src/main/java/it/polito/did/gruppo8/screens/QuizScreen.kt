d77c02e9b363adee8698dc2d74e3e5ae8b86ac66
Edoardo <edoardo.aralla@icloud.com>
2023-02-18 15:16:14 +0100

    Aggiunto layout per schermata quiz. Manca aggiunta del timer

--- gioco/app/src/main/java/it/polito/did/gruppo8/screens/QuizScreen.kt
+++ gioco/app/src/main/java/it/polito/did/gruppo8/screens/QuizScreen.kt
@@ -1,8 +1,18 @@
 package it.polito.did.gruppo8.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import it.polito.did.gruppo8.GameViewModel
 
@@ -11,8 +21,106 @@ import it.polito.did.gruppo8.GameViewModel
 
 @Composable
 fun QuizScreen(modifier: Modifier = Modifier) {
-    val vm: GameViewModel = viewModel()
-    val players = vm.players.observeAsState()
+        Box(modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+        ){
+            Column(modifier = Modifier.fillMaxWidth()
+                .padding(16.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                QuizCard()
+                Spacer(modifier = Modifier.size(16.dp))
+                Button(onClick = { /*TODO*/ }) {
+                    Text(text = "Salta domanda")
+                }
+            }
+        }
+    }
 
-//TODO
+@Composable
+//Bisogna passare alla funzione testo della domanda, testi delle risposte per popolare gli elementi
+//Occorre anche passare risposta corretta al pulsante giusto
+fun QuizCard() {
+    Card(
+        modifier = Modifier.fillMaxWidth(),
+        backgroundColor = Color.DarkGray,
+        elevation = 5.dp,
+        shape = RoundedCornerShape(30.dp)
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(300.dp)
+                        .padding(16.dp),
+                    backgroundColor = Color.Yellow,
+                    elevation = 5.dp,
+                    shape = RoundedCornerShape(10.dp)
+                ) {
+                    Text(text = "Questa è la domanda del quiz che deve essere pescata dal database",
+                        Modifier.padding(16.dp))
+                }
+                Spacer(modifier = Modifier.size(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Risposta 1")
+                    }
+                    Spacer(modifier = Modifier.size(16.dp))
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Risposta 2")
+                    }
+                }
+                Spacer(modifier = Modifier.size(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Risposta 3")
+                    }
+                    Spacer(modifier = Modifier.size(16.dp))
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Risposta 4")
+                    }
+                }
+                Spacer(modifier = Modifier.size(16.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                ) {
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Risposta 5")
+                    }
+                    Spacer(modifier = Modifier.size(16.dp))
+                    Button(onClick = { /*TODO*/ }) {
+                        Text(text = "Risposta 6")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun QuizScreenPreview() {
+    QuizScreen()
 }
