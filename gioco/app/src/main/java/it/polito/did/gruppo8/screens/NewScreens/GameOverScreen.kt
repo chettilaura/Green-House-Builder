f8063245fad20b95b23b5ed76c48b852b5b2e4be
giomerco <92179949+giomerco@users.noreply.github.com>
2023-06-07 15:53:16 +0200

    Aggiunta podio GameOverScreen

--- gioco/app/src/main/java/it/polito/did/gruppo8/screens/NewScreens/GameOverScreen.kt
+++ gioco/app/src/main/java/it/polito/did/gruppo8/screens/NewScreens/GameOverScreen.kt
@@ -1,13 +1,20 @@
 package it.polito.did.gruppo8.screens.NewScreens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
@@ -18,15 +25,18 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import it.polito.did.gruppo8.R
 import it.polito.did.gruppo8.model.baseClasses.Player
 import it.polito.did.gruppo8.screens.caveatBold
+import it.polito.did.gruppo8.ui.theme.DarkGreen
 import it.polito.did.gruppo8.util.myComposable.MyPlayerData
 
-//SCHERMAT VISIBILE AL TERMINE DELLA PARTITA
+//SCHERMATA VISIBILE AL TERMINE DELLA PARTITA
 
 @Composable
 fun GameOverScreen(modifier: Modifier = Modifier) {
@@ -44,6 +54,137 @@ fun GameOverScreen(modifier: Modifier = Modifier) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally) {
             GameOverTopBar(title = "GAME OVER ", colorId = colorResource(id = R.color.cal_poly_green))
+
+            //PRIMO POSTO
+            Card(modifier = Modifier
+//                .width(120.dp)
+//                .height(80.dp)
+                .padding(10.dp),
+                elevation = 4.dp,
+                shape = RoundedCornerShape(15.dp),
+                border = BorderStroke(3.dp, Color(0xFFDAA520))
+            ) {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.15f),
+                    /*.padding(1.dp),*/
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+
+                    Image(
+                        painter = painterResource(R.drawable.coccarda1),
+                        contentDescription = "primo posto",
+                        //modifier = Modifier.size(50.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                //TODO: verificare che funzioni prendendo i primi 3 player dalla lista players
+
+//                Text(
+//                    text = players[1].nickname,
+//                    fontFamily = caveatBold,
+//                    color = DarkGreen,
+//                    style = MaterialTheme.typography.h3,
+//                    textAlign = TextAlign.Center
+//                )
+
+
+
+                }
+            }
+
+            //SECONDO POSTO
+            Card(modifier = Modifier
+//                .width(120.dp)
+//                .height(80.dp)
+                .padding(10.dp),
+                elevation = 4.dp,
+                shape = RoundedCornerShape(15.dp),
+                border = BorderStroke(3.dp, Color(0xFFC0C0C0))
+            ) {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.15f),
+                    /*.padding(1.dp),*/
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+
+                    Image(
+                        painter = painterResource(R.drawable.coccarda2),
+                        contentDescription = "primo posto",
+                        //modifier = Modifier.size(50.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                //TODO: verificare che funzioni prendendo i primi 3 player dalla lista players
+
+//                Text(
+//                    text = players[1].nickname,
+//                    fontFamily = caveatBold,
+//                    color = DarkGreen,
+//                    style = MaterialTheme.typography.h3,
+//                    textAlign = TextAlign.Center
+//                )
+
+
+
+
+
+                }
+            }
+
+            //TERZO POSTO
+            Card(modifier = Modifier
+//                .width(120.dp)
+//                .height(80.dp)
+                .padding(10.dp),
+                elevation = 4.dp,
+                shape = RoundedCornerShape(15.dp),
+                border = BorderStroke(3.dp, Color(0xFFCD7F32))
+            ) {
+                Row(modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight(0.15f),
+                    /*.padding(1.dp),*/
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+
+                    Image(
+                        painter = painterResource(R.drawable.coccarda3),
+                        contentDescription = "primo posto",
+                        //modifier = Modifier.size(50.dp),
+                        contentScale = ContentScale.Fit
+                    )
+
+                //TODO: verificare che funzioni prendendo i primi 3 player dalla lista players
+//
+//                Text(
+//                    text = players[2].nickname,
+//                    fontFamily = caveatBold,
+//                    color = DarkGreen,
+//                    style = MaterialTheme.typography.h3,
+//                    textAlign = TextAlign.Center
+//                )
+
+
+
+                }
+            }
+
+
+
+            Text(
+                text = "OVERALL",
+                fontFamily = caveatBold,
+                color = DarkGreen,
+                style = MaterialTheme.typography.h4,
+                textAlign = TextAlign.Center
+            )
+
+            //mostrata classifica generale (come in dashboard)
 
             Column(
                 verticalArrangement = Arrangement.Center,
